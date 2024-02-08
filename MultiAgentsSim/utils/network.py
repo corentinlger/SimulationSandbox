93dd22b486d2522a032568af36f3f39863a883ef
Marsolo1 <martialmarzloff@gmail.com>
2024-02-08 14:04:30 +0100

    added localhost to server list

--- MultiAgentsSim/utils/network.py
+++ MultiAgentsSim/utils/network.py
@@ -1,8 +1,8 @@
 import pickle
 import socket 
 
-servers = ['10.204.2.229', '10.204.2.189', '192.168.1.24']
-server_idx = 0
+servers = ['10.204.2.229', '10.204.2.189', '192.168.1.24', 'localhost']
+server_idx = 3
 
 SERVER = servers[server_idx]
 
